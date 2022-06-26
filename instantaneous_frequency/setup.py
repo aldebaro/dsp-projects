9d7ce885159a35ab9464669e06d951c91e3f7212
Wilson Cosmo <97473599+WCosmo@users.noreply.github.com>
2022-06-26 00:54:16 -0300

    Update setup.py

--- instantaneous_frequency/setup.py
+++ instantaneous_frequency/setup.py
@@ -10,7 +10,6 @@ Script description: This script installs the libraries for the project and downl
 import os
 import sys
 import subprocess
-from google_drive_downloader import GoogleDriveDownloader as gdd
 
 print('\nStarting library install\n----------------------------------------\n')
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
@@ -23,6 +22,9 @@ reqs = subprocess.check_output ([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 print(installed_packages)
 print('\nLibrary install done\n----------------------------------------\n')
+
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
 print('\nStarting data download\n----------------------------------------\n')
 gdd.download_file_from_google_drive(file_id='15Z6ZK2TH3-0JvE0mcMBtsbmmsljafs1L', dest_path='./test_signals.zip', unzip=True)
 os.remove('./test_signals.zip')
