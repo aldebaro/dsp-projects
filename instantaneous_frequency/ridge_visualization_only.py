15bf9579db89a04763ca0d0ff2b011287003f77f
Wilson Cosmo <97473599+WCosmo@users.noreply.github.com>
2022-06-26 00:13:14 -0300

    Update ridge_visualization_only.py

--- instantaneous_frequency/ridge_visualization_only.py
+++ instantaneous_frequency/ridge_visualization_only.py
@@ -1,3 +1,12 @@
+'''
+DSP - Final Project
+Project Name: Extraction of instantaneous frequencies using Synchrosqueezing in Python
+Student: Wilson Cosmo
+Date: 22/06/2022
+
+Script description: This script plots ridge curves visualization of a loaded .wav signal. The ridge curves are extracted from CWT Transform and SSQ CWT.
+Input Parameters: --sf 'path_to_file.wav' --p 'penalty for frequency change' --nr 'number of ridges'
+'''
 
 import numpy as np
 import scipy.signal as sig
@@ -7,14 +16,13 @@ from scipy.io import wavfile as wav
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--sfile", type=str, default='./signals/wav/dolphins/SanctSound_CI01_03_dolphins_20190904T064203Z.wav', help="path to the signal file")
-parser.add_argument("--t", type=str, default='wav', help="file type (wav, npy)")
-parser.add_argument("--penalty", type=int, default=60000, help="penalty for frequency change")
-parser.add_argument("--nridges", type=int, default=3, help="#number of ridges")
+parser.add_argument("--sf", type=str, default='./test_signals/visualization/dolphins/SanctSound_CI01_03_dolphins_20190904T064203Z.wav', help="path to the signal file")
+parser.add_argument("--p", type=int, default=60000, help="penalty for frequency change")
+parser.add_argument("--nr", type=int, default=3, help="number of ridges")
 opt = parser.parse_args()
 print(opt)
 
-#Visual method
+# Visual method
 def viz(x, Tf, ridge_idxs, yticks=None, tt = 'Signal', yl = 'Frequency Scale'):
     ylabel = (yl)
     title = (tt)
@@ -23,16 +31,11 @@ def viz(x, Tf, ridge_idxs, yticks=None, tt = 'Signal', yl = 'Frequency Scale'):
     imshow(Tf, **ikw, show=0)
     plot(ridge_idxs, **pkw, show=1)
 
-penalty = opt.penalty
-nr = opt.nridges #n of ridges
+penalty = opt.p
+nr = opt.nr #n of ridges
 
-#load signal
-if opt.t == 'wav':
-    (rate,sig) = wav.read(opt.sfile)
-elif opt.t == 'npy':
-    sig = np.load(opt.sfile)
-else:
-    print('File extension not supported.')
+# load signal
+(rate,sig) = wav.read(opt.sf)
 
 x = sig
 t = int(sig.size)
@@ -43,6 +46,6 @@ Tx, Wx, ssq_freqs, scales = ssq_cwt(x)
 ridge_idxs = extract_ridges(Wx, scales, penalty, n_ridges=nr, bw=25)
 viz(x, Wx, ridge_idxs, scales, tt = 'CWT Ridges')
 
-# SSQ_CWT example (note the jumps)
+# SSQ CWT
 ridge_idxs = extract_ridges(Tx, scales, penalty, n_ridges=nr, bw=4)
 viz(x, Tx, ridge_idxs, ssq_freqs, tt = 'CWT with SSQ Ridges')
