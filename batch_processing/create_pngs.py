b415e28110746640391012e9b2582108e73ea49f
valdinei.conceicao@itec.ufpa.br <97#_PSy*JG8eDgn>
2023-01-23 11:10:28 -0300

    Comments the code to explain better her functionality

--- batch_processing/create_pngs.py
+++ batch_processing/create_pngs.py
@@ -1,3 +1,22 @@
+'''
+First, creates a argument parser to interpret command line arguments, utilized to know the output folder to store the images generated (-o or --o <folder>).
+If the output folder already exists, the program stop to prevent overwriting files.
+Then, search recursively all files with .wav extension and sub-directories, then store the paths of these files.
+After that the code process each file to read the signal waveform and saves the plot in a png file on the choosen output folder.
+
+The main module to read signals from the .wav files is the built in python wave, considering that we don't need to install anything new
+this module was the best choice to begginers in python. As other options to extract the waveform from the files, we have the modules
+scipy.wave or wavy (https://pypi.org/project/wavy/)
+
+Credits:
+- Dataset utilized to tests: https://www.kaggle.com/datasets/lazyrac00n/speech-activity-detection-datasets?resource=download
+
+Authors:
+- Giovanna Cunha
+- Gabriel Moraes
+- Valdinei Rodrigues
+'''
+
 import os
 import wave
 import numpy as np
@@ -7,24 +26,29 @@ import argparse
 wavFiles = []
 amplitudes = []
 
+# Creates argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output')
 parser.parse_args('--output 1'.split())
 
+# Gets the args from the parses
 args = vars(parser.parse_args())
 
 input_folder = 'DataSet'
 output_folder = args['output']
 
+# If the output folder already exists, the code stops
 if(os.path.exists('../' + output_folder)): 
     print('Output folder already exists!')
     exit()
 
+# Search reursively the files and store in a list
 for root, dirs, files in os.walk(f'../{input_folder}/'):
     for file in files:
         if (file.endswith('.wav')):
             wavFiles.append(os.path.join(root, file))
 
+# Function to get the waveform data of each file
 def getAudioData(wav_obj):
     n_samples = wav_obj.getnframes()
     sample_freq = wav_obj.getframerate()
@@ -35,27 +59,25 @@ def getAudioData(wav_obj):
     return audioData
 
 for wavFile in wavFiles:
+    # Here we could have utilized other options like scipy.wave or wavy
     wav_obj = wave.open(wavFile)
     audioData = getAudioData(wav_obj)
 
-    min_aplitude = audioData['signal_array'].min()
-    max_aplitude = audioData['signal_array'].max()
-    mean_aplitude = audioData['signal_array'].mean()
-
+    # Creates the plot of current file waveform
     plt.ylabel('Amplitude value')
     plt.title(f'Amplitude histogram of {wavFile}')
     plt.hist(audioData['signal_array'])
 
+    # Sets the output folder, utilizing the same folder structure but in a different output "root" folder
     output_file = wavFile.replace(input_folder, output_folder)
     output_file = output_file.replace('.wav', '.png')
     output_file_folder = output_file.split('/')
     del output_file_folder[-1]
     output_file_folder = '/'.join(output_file_folder)
 
-    #print(output_file_folder)
-
+    # If the path doesn't exists, create
     if not (os.path.exists(output_file_folder)): os.makedirs(output_file_folder)
 
-    #print(output_file)
+    # Save the plot in a png file to the output folder
     plt.savefig(output_file)
     plt.close()
--- batch_processing/display_histograms.py
+++ batch_processing/display_histograms.py
@@ -1,3 +1,22 @@
+'''
+Search recursively all files with .wav extension and sub-directories, then store the paths of these files.
+After that the code process each file to read the signal waveform and then find the duration in seconds.
+With this data, we describe some statistcs of each file, containing the duration and minimum, maximum and mean amplitude,
+displaying the amplitude to each waveform and in the end we describe the minimum, maximum and mean duration of the files.
+
+The main module to read signals from the .wav files is the built in python wave, considering that we don't need to install anything new
+this module was the best choice to begginers in python. As other options to extract the waveform from the files, we have the modules
+scipy.wave or wavy (https://pypi.org/project/wavy/)
+
+Credits:
+- Dataset utilized to tests: https://www.kaggle.com/datasets/lazyrac00n/speech-activity-detection-datasets?resource=download
+
+Authors:
+- Giovanna Cunha
+- Gabriel Moraes
+- Valdinei Rodrigues
+'''
+
 import os
 import wave
 import numpy as np
@@ -6,6 +25,7 @@ import matplotlib.pyplot as plt
 wavFiles = []
 amplitudes = []
 
+# Search reursively the files and store in a list
 for root, dirs, files in os.walk('../DataSet/'):
     for file in files:
         if (file.endswith('.wav')):
@@ -13,6 +33,7 @@ for root, dirs, files in os.walk('../DataSet/'):
 
 durationTimes = np.empty(len(wavFiles))
 
+# Function to get the waveform data of each file
 def getAudioData(wav_obj):
     n_samples = wav_obj.getnframes()
     sample_freq = wav_obj.getframerate()
@@ -23,14 +44,17 @@ def getAudioData(wav_obj):
     return audioData
 
 for wavFile in wavFiles:
+    # Here we could have utilized other options like scipy.wave or wavy
     wav_obj = wave.open(wavFile)
     audioData = getAudioData(wav_obj)
 
+    # Find the duration of the audio file in seconds, storing in a list
     audioData['duration'] = audioData['n_samples'] / float(audioData['samples_freq'])
     duration = audioData['duration']
-    #durationTimes.append(audioData['duration'])
+
     np.append(durationTimes, audioData['duration'])
 
+    # Describes min, max and mean amplitude of current file
     min_aplitude = audioData['signal_array'].min()
     max_aplitude = audioData['signal_array'].max()
     mean_aplitude = audioData['signal_array'].mean()
@@ -41,11 +65,13 @@ for wavFile in wavFiles:
     print(f'Max amplitute: {max_aplitude}')
     print(f'Mean amplitute: {mean_aplitude}')
 
+    # Plot a histogram of amplitude 
     plt.ylabel('Amplitude value')
     plt.title(f'Amplitude histogram of {wavFile}')
     plt.hist(audioData['signal_array'])
     plt.show()
 
+# Describes min, max and mean duration for all files in dataset
 print(f'\nMin duration: {min(durationTimes)}s')
 print(f'Max duration: {max(durationTimes)}s')
 print(f'Mean duration: {np.nanmean(durationTimes)}s')
