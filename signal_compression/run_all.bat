d9518095f2b346625f01c03aee74fe3eda6a7218
1176325 <1176325@gitlab.lasse.ufpa.br>
2023-04-09 12:42:43 -0300

    Script for Windows to generate all examples

---
+++ signal_compression/run_all.bat
@@ -0,0 +1,10 @@
+python type_1_signal.py
+python type_2_signal.py
+python type_3_signal.py
+python type_4_signal.py
+python type_5_signal.py
+python type_6_signal.py
+
+move compressed*bits ./examples
+move original*double ./examples
+move original_6.txt ./examples
